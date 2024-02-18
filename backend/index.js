57d80e2135e8f261ada0b17aef450f2b90bf16d3
HRISHIKESH SAPKAL <132556772+HRISHIXP@users.noreply.github.com>
2024-02-18 18:57:25 +0530

    hrishi

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 app.use(cors());
-
+require ("dotenv").config()
 const server = http.createServer(app);
 
 const io = new Server(server, {
@@ -31,6 +31,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
+server.listen(process.env.PORT||5000, () => {
   console.log("SERVER RUNNING");
 });
